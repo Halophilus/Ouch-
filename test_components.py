96fc511405206d88251f1eacf98d1b419414d090
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-08 17:20:39 -0400

    Update test_components.py
    Add safety features to turn off LEDs and other devices when script is done in the event of an abrupt interruption

--- test_components.py
+++ test_components.py
@@ -4,6 +4,16 @@ import time
 def wait_for_input():
     input("Press ENTER to continue...")
 
+def cleanup():
+    print("Turning off all devices...")
+    power.off()
+    monitor.off()
+    powerLight.off()
+    activity.off()
+    front_fan.off()
+    rear_fan.off()
+    buzzer.off()
+
 # LEDs
 power = LED(14)
 monitor = LED(15)
@@ -25,87 +35,83 @@ key_button = Button(25, pull_up=True, debounce_time=0.2)
 
 # Distance sensor
 distance_sensor = DistanceSensor(echo=20, trigger=21)
-
-# Test power LED
-print("Testing power LED...")
-power.on()
-wait_for_input()
-
-# Test monitor LED
-print("Testing monitor LED...")
-monitor.on()
-wait_for_input()
-
-# Test powerLight LED
-print("Testing powerLight LED...")
-colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
-for color in colors:
-    powerLight.color = color
-    print(f"PowerLight color: {color}")
-    time.sleep(1)
-powerLight.color = (1, 1, 1)
-wait_for_input()
-
-# Test activity LED
-print("Testing activity LED...")
-for _ in range(5):
-    activity.toggle()
-    time.sleep(1)
-activity.on()
-wait_for_input()
-
-# Test fans
-print("Testing front fan...")
-for i in range(6):
-    front_fan.value = i / 5
-    time.sleep(1)
-front_fan.off()
-wait_for_input()
-
-print("Testing rear fan...")
-for i in range(6):
-    rear_fan.value = i / 5
-    time.sleep(1)
-rear_fan.off()
-wait_for_input()
-
-# Test buzzer
-print("Testing buzzer...")
-for _ in range(3):
-    buzzer.on()
-    time.sleep(0.2)
-    buzzer.off()
-    time.sleep(0.2)
-wait_for_input()
-
-# Test buttons
-def button_pressed(button_name):
-    print(f"{button_name} button pressed")
-
-def button_released(button_name):
-    print(f"{button_name} button released")
-
-buttons = [("black", black_button), ("yellow", yellow_button), ("red", red_button), ("key", key_button)]
-
-for button_name, button in buttons:
-    print(f"Press {button_name} button...")
-    button.when_pressed = lambda: button_pressed(button_name)
-    button.when_released = lambda: button_released(button_name)
-    button.wait_for_release()
-    time.sleep(0.5)
+try:
+    # Test power LED
+    print("Testing power LED...")
+    power.on()
     wait_for_input()
 
-# Test distance sensor
-print("Testing distance sensor...")
-for i in range(1, 6):
-    print(f"Move object to {i} feet away...")
-    while distance_sensor.distance * 3.281 < i - 0.1 or distance_sensor.distance * 3.281 > i + 0.1:
-        time.sleep(0.1)
-    print(f"Object detected at {i} feet")
+    # Test monitor LED
+    print("Testing monitor LED...")
+    monitor.on()
     wait_for_input()
 
-# Turn off everything
-print("Turning off everything...")
-power.off()
-monitor.off()
-powerLight.off()
+    # Test powerLight LED
+    print("Testing powerLight LED...")
+    colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+    for color in colors:
+        powerLight.color = color
+        print(f"PowerLight color: {color}")
+        time.sleep(1)
+    powerLight.color = (1, 1, 1)
+    wait_for_input()
+
+    # Test activity LED
+    print("Testing activity LED...")
+    for _ in range(5):
+        activity.toggle()
+        time.sleep(1)
+    activity.on()
+    wait_for_input()
+
+    # Test fans
+    print("Testing front fan...")
+    for i in range(6):
+        front_fan.value = i / 5
+        time.sleep(1)
+    front_fan.off()
+    wait_for_input()
+
+    print("Testing rear fan...")
+    for i in range(6):
+        rear_fan.value = i / 5
+        time.sleep(1)
+    rear_fan.off()
+    wait_for_input()
+
+    # Test buzzer
+    print("Testing buzzer...")
+    for _ in range(3):
+        buzzer.on()
+        time.sleep(0.2)
+        buzzer.off()
+        time.sleep(0.2)
+    wait_for_input()
+
+    # Test buttons
+    def button_pressed(button_name):
+        print(f"{button_name} button pressed")
+
+    def button_released(button_name):
+        print(f"{button_name} button released")
+
+    buttons = [("black", black_button), ("yellow", yellow_button), ("red", red_button), ("key", key_button)]
+
+    for button_name, button in buttons:
+        print(f"Press {button_name} button...")
+        button.when_pressed = lambda: button_pressed(button_name)
+        button.when_released = lambda: button_released(button_name)
+        button.wait_for_release()
+        time.sleep(0.5)
+        wait_for_input()
+
+    # Test distance sensor
+    while True:
+        distance_in_feet = distance_sensor.distance * 3.281
+        print(f"Object detected at {distance_in_feet:.2f} feet")
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Stopping script.")
+
+finally:
+    cleanup()
