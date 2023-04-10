877ee603ffa45cff20d64b9e4463f633a7fdc9c8
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-10 01:55:40 -0400

    Update VideoPlayer.py

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -1,25 +1,24 @@
 import os
 import time
-from omxplayer.player import OMXPlayer
-from pathlib import Path
+import subprocess
 
 class InteractiveVideoPlayer:
     def __init__(self, video_files, audio_output="local"):
         self.video_files = video_files
         self.current_video_index = 0
-        self.audio_output = audio_output
-        self.player = None
+        self.audio_output = "hdmi" if audio_output == "hdmi" else "local"
+        self.player_process = None
 
     def play_video(self, video_index=None):
         if video_index is not None:
             self.current_video_index = video_index
 
         video_path = self.video_files[self.current_video_index]
-        if self.player is not None:
-            self.player.quit()
+        if self.player_process is not None and self.player_process.poll() is None:
+            self.player_process.terminate()
 
-        self.player = OMXPlayer(video_path, args=['--loop', '--no-osd', f'--adev={self.audio_output}'])
-        self.player.set_video_pos(0, 0, 1920, 1080)
+        args = ['omxplayer', '--loop', '--no-osd', f'--adev={self.audio_output}', video_path]
+        self.player_process = subprocess.Popen(args)
 
     def next_video(self):
         self.current_video_index += 1
