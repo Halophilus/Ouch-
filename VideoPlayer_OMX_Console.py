ac95dea0379f6b5a62455f1e4620cd81c2682d93
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-10 03:03:00 -0400

    Add VLC VideoPlayer

--- VideoPlayer_OMX_Console.py
+++ VideoPlayer_OMX_Console.py
@@ -3,43 +3,44 @@ import time
 import threading
 
 
-class VLCPlayerEngine:
+class OMXPlayerEngine:
     def __init__(self, video_paths):
         self.video_paths = video_paths
         self.current_index = 0
-        self.vlcplayer_process = None
+        self.omxplayer_process = None
         self.loop_video = True
         self.video_loop_thread = None
 
     def _loop_video(self):
         while self.loop_video:
-            if self.vlcplayer_process is not None and self.vlcplayer_process.poll() is not None:
+            if self.omxplayer_process is not None and self.omxplayer_process.poll() is not None:
                 self.play_video(self.current_index)
             time.sleep(0.5)
 
     def play_video(self, index):
-        if self.vlcplayer_process is not None and self.vlcplayer_process.poll() is None:
-            self.vlcplayer_process.terminate()
+        if self.omxplayer_process is not None and self.omxplayer_process.poll() is None:
+            self.omxplayer_process.terminate()
             time.sleep(0.5)
 
         if 0 <= index < len(self.video_paths):
             self.current_index = index
             video_path = self.video_paths[self.current_index]
-            self.vlcplayer_process = subprocess.Popen(
+            self.omxplayer_process = subprocess.Popen(
                 [
-                    "cvlc",
+                    "omxplayer",
                     "--no-osd",
-                    "--no-xlib",
-                    "--fullscreen",
-                    "--no-video-title-show",
-                    "--loop",
-                    "--aout=alsa",
-                    "--alsa-audio-device=hw:1,0",
+                    "--no-keys",
+                    "-b",
+                    "--adev",
+                    "local",
                     video_path,
                 ],
                 stdout=subprocess.DEVNULL,
                 stderr=subprocess.DEVNULL,
             )
+            if self.video_loop_thread is None:
+                self.video_loop_thread = threading.Thread(target=self._loop_video)
+                self.video_loop_thread.start()
         else:
             print(f"Error: Invalid index {index}. Video not found.")
 
@@ -54,7 +55,7 @@ class VLCPlayerEngine:
 
 
 if __name__ == "__main__":
-    video_player = VLCPlayerEngine(["video1.mp4", "video2.mp4", "video3.mp4"])
+    video_player = OMXPlayerEngine(["video1.mp4", "video2.mp4", "video3.mp4"])
 
     # Play the video at index 1
     video_player.play_video(1)
