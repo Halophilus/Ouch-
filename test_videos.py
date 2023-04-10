74f4f8aa32e579d0082f9e421b6852881f4fd9f6
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-09 21:18:32 -0400

    Increase video framerate
    All videos upped to 25FPS

--- test_videos.py
+++ test_videos.py
@@ -1,6 +1,8 @@
 from gpiozero import LED, Button
-from VideoPlayer import OmxplayerPlayer
+from VideoPlayer import VideoEngine
 from time import sleep
+from pathlib import Path
+
 
 monitor = LED(15)
 power = LED(14)
@@ -9,59 +11,60 @@ yellow_button = Button(1, pull_up=True, hold_time=0.2, hold_repeat=True)
 red_button = Button(7, pull_up=True, hold_time=0.2, hold_repeat=True)
 key_button = Button(25, pull_up=True, hold_time=0.2, hold_repeat=True)
 
-player = OmxplayerPlayer()
-
-def timeRemaining():
-    t = player.get_time_remaining
-    sleep(t)
-
-player.show_default_image()
+player = VideoEngine()
 
 monitor.on()
-
-player.play("STARTUP_LOOP.MP4", loop=True)
+power.on()
+player.play_video(Path("/home/pi/Ouch-/STARTUP_LOOP.mp4"), loop=True)
 
 key_button.wait_for_press()
 
-player.play("Main Sequence/1.mp4")
+player.replace_video("/home/pi/Ouch-/Main Sequence/1.mp4")
 
-timeRemaining()
+t = player.get_time_remaining()
+sleep(t)
 
-player.play("Transitions/1.mp4", loop = True)
+player.replace_video("/home/pi/Ouch-/Transitions/1.mp4", loop = True)
 
 black_button.wait_for_press()
 
-player.play("Button Press/BLACK.mp4", loop = True)
+player.replace_video("/home/pi/Ouch-/Button Press/BLACK.mp4", loop = True)
 
 black_button.wait_for_release()
 
-player.play("Main Sequence/2.mp4")
+player.replace_video("/home/pi/Ouch-/Main Sequence/2.mp4")
 
-timeRemaining()
+t = player.get_time_remaining()
+sleep(t)
 
-player.play("Transitions/2.mp4", loop = True)
-
-yellow_button.wait_for_press()
-
-player.play("Button Press/YELLOW.mp4", loop = True)
-
-yellow_button.wait_for_release()
-
-player.play("Main Sequence/3.mp4")
-
-timeRemaining()
-
-player.play("Transitions/3.mp4", loop = True)
+player.replace_video("/home/pi/Ouch-/Transitions/2.mp4", loop = True)
 
 yellow_button.wait_for_press()
 
-player.play("Button Press/RED.mp4", loop = True)
+player.replace_video("/home/pi/Ouch-/Button Press/YELLOW.mp4", loop = True)
 
 yellow_button.wait_for_release()
 
-player.play("CREDITS.MP4")
-timeRemaining()
-player.set_default_image("SHUTDOWN_FRAME.PNG")
+player.replace_video("/home/pi/Ouch-/Main Sequence/3.mp4")
+
+t = player.get_time_remaining()
+sleep(t)
+
+player.replace_video("/home/pi/Ouch-/Transitions/3.mp4", loop = True)
+
+yellow_button.wait_for_press()
+
+player.replace_video("/home/pi/Ouch-/Button Press/RED.mp4", loop = True)
+
+yellow_button.wait_for_release()
+
+player.replace_video("/home/pi/Ouch-/CREDITS.mp4")
+
+t = player.get_time_remaining()
+sleep(t)
+
+player.display_image("/home/pi/Ouch-/SHUTDOWN_FRAME.png")
 key_button.wait_for_release
+player.stop()
 monitor.off()
 power.off()
