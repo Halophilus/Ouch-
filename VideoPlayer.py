f083e88fe36361017533cbcb4424bef1ca0fbaee
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-13 15:18:32 -0400

    Add 'self' args to _create_section_dictionary

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -19,7 +19,7 @@ class VLCVideoPlayer:
         if file_path is not None:
             self._play_video(file_path)
     
-    def _create_section_dictionary(sections):
+    def _create_section_dictionary(self, sections):
         tracking_point = 0.0
         section_dict = {}
 
