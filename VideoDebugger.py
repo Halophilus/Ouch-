28bdc0d04f48e7be88929d0727d958b5ff8f88e5
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-14 13:38:07 -0400

    Update VideoDebugger.py

--- VideoDebugger.py
+++ VideoDebugger.py
@@ -1,19 +1,14 @@
 import vlc
 import time
 
-def vlc_log_handler(data, level, ctx, fmt, args):
-    msg = fmt % args
-    print("[VLC] %s" % msg)
-
-# Initialize VLC instance with log handler
+# Initialize VLC instance
 instance = vlc.Instance()
-instance.log_set(vlc_log_handler, None)
 
 # Create a media player object
 player = instance.media_player_new()
 
 # Load the media (replace 'path/to/your/video.mp4' with the actual file path)
-media = instance.media_new('/home/pi/Ouch-/master.mp4')
+media = instance.media_new('path/to/your/video.mp4')
 
 # Set the media to the player instance
 player.set_media(media)
@@ -24,13 +19,14 @@ player.play()
 # Wait for the video to start playing
 time.sleep(1)
 
-# Check if the video is playing
-if player.get_state() == vlc.State.Playing:
-    # Set the time to 10 seconds (10,000 milliseconds)
-    player.set_time(10000)
-    print("Successfully set the time to 10 seconds.")
-else:
-    print("Video is not playing yet.")
+# Jump to the 10th second while the video is playing
+player.set_time(10000)
+
+# Wait for 5 seconds
+time.sleep(5)
+
+# Jump to the 30th second (30,000 milliseconds) while the video is still playing
+player.set_time(30000)
 
 # Wait for 10 seconds before stopping the video
 time.sleep(10)
