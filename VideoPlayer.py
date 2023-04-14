229c4930d0d0f38a8382f60ea61e4eea9a6eb002
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-14 17:19:14 -0400

    Update VideoPlayer.py

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -2,22 +2,47 @@ import vlc
 import time
 import threading
 
+class VLCLooper:
+    def __init__(self, *, media, start_time, end_time):
+        self._media = media
+        self._start_time = start_time
+        self._end_time = end_time
+        self._kill = False
+        self._task = None
+
+    def start(self):
+        if self._task:
+            raise Exception("Already Started")
+        else:
+            self._task = threading.Thread(target=self._loop)
+            self._task.start()
+
+    def _loop(self):
+        while not self._kill:
+            self._media.set_time(self._start_time)
+            if self._media.get_time() >= self._end_time:
+                self._media.set_time(self._start_time)
+            time.sleep(0.1)
+
+    def stop(self):
+        self._kill = True
+        self._task.join()
+        self._task = None
+
 class VLCVideoPlayer:
     def __init__(self, video_list=[],file_path=None):
         self.player = None
         self.video_list = video_list
         self.section_dict = None
-        self.section_index_list = None
         self.stop_loop = False
-        self.current_thread = None
-        self.stop_loop_event = threading.Event()
 
-        if self.video_list is not None:
-            self.section_dict = self.create_section_dictionary(self.video_list)
-            self.section_index_list = self.create_section_index_list(self.video_list)
-
-        if file_path is not None:
-            self.play_video(file_path)
+        if not self.video_list:
+            raise Exception("Empty video list")
+        if not file_path:
+            raise Exception("No file path")
+        
+        self.section_dict = self.create_section_dictionary(self.video_list)
+        self.play_video(file_path)
     
     def create_section_dictionary(self, sections):
         tracking_point = 0.0
@@ -37,35 +62,23 @@ class VLCVideoPlayer:
         self.player.set_media(media)
         self.player.audio_output_set("analog")
         self.player.play()
-        first_video = self.section_index_list[0]
-        first_video = self.section_dict[first_video]
-        self.play_section(first_video)
+        self.looper = VLCLooper(media=media, start_time = 0, end_time=4000)
+        self.looper.start()
+        time.sleep(10000)
+        #first_video = self.section_index_list[0]
+        #first_video = self.section_dict[first_video]
+        #self.play_section(first_video)
 
     def play_video_from_time_point(self, time_point):
-        if self.player is None:
-            print("No video!")
-            return
-        if self.player.get_state() in [vlc.State.Playing, vlc.State.Paused]:
-            # Convert time_point to milliseconds and set the player's time
-            self.player.set_time(int(time_point * 1000))
-        else:
-            print("Out of range!")
+        pass
 
     def play_section(self, section_name):
         if section_name not in self.section_dict:
             print("Section not found!")
             return
 
-        # Stop the current loop
-        self.stop_loop = True
-        self.stop_loop_event.wait()  # Wait for the section_loop to signal that it has stopped
-
-        if self.current_thread is not None:
-            self.current_thread.join()
-
         # Reset the stop_loop flag and the stop_loop_event
         self.stop_loop = False
-        self.stop_loop_event.clear()
 
         def section_loop():
             start_time, end_time = self.section_dict[section_name]
@@ -82,10 +95,6 @@ class VLCVideoPlayer:
         # Start the loop in a background thread
         self.current_thread = threading.Thread(target=section_loop)
         self.current_thread.start()
-
-    def create_section_index_list(self, sections):
-        section_index_list = [section_name for section_name, _ in sections]
-        return section_index_list
 
     def stop(self):
         # Stop the current section loop if it's running
