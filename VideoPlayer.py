2802cfb0ea3db9ad9a8fe890d64e011cecb5aeb7
Halophilus <33325217+Halophilus@users.noreply.github.com>
2023-04-14 17:39:25 -0400

    Update VideoPlayer.py

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -26,7 +26,7 @@ class VLCLooper:
         while not self._kill:
             print("SETTING TIME")
             print(f"GOT TIME: {self._media.get_time()}" )
-            print("END TIME: {self._end_time}")
+            print(f"END TIME: {self._end_time}")
             if self._media.get_time() >= self._end_time:
                 self._media.set_time(self._start_time)
             time.sleep(0.1)
